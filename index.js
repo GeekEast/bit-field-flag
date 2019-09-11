b5fdf9d84e73c42a6bec13cc1df9e872cc1942eb
GeekEast <sx.ba2017@gmail.com>
2019-09-11 11:24:58 +0930

    init

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // flag to literal
-const checkNPosition = (f, nth) => (f >> nth) & 1;
+const checkNPosition = (f, nth) => (f >> (nth - 1)) & 1;
+console.log(checkNPosition(10, 4));
 
 // literal to flag
 const getFlag = binary => parseInt(binary, 2);
+console.log(getFlag('1010'));
